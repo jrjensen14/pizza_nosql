9b3feed76da3fc3c8c9674f97e8fe849388bfda9
JRJ <jrjensen14@gmail.com>
2021-04-15 21:13:39 -0500

    working on pizza comments

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -20,19 +20,18 @@ const pizzaController = {
   // get one pizza by id
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
-      .then(dbPizzaData => {
-        // If no pizza is found, send 404
-        if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
-          return;
-        }
-        res.json(dbPizzaData);
+      .populate({
+        path: 'comments',
+        select: '-__v'
       })
+      .select('-__v')
+      .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
-        res.status(400).json(err);
+        res.sendStatus(400);
       });
   },
+
   // createPizza
   createPizza({ body }, res) {
     Pizza.create(body)
@@ -41,7 +40,11 @@ const pizzaController = {
   },
   // update pizza by id
   updatePizza({ params, body }, res) {
-    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+    Pizza.findOneAndUpdate(
+      { _id: params.id }, 
+      body, 
+      { new: true }
+      )
       .then(dbPizzaData => {
         if (!dbPizzaData) {
           res.status(404).json({ message: 'No pizza found with this id!' });
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,23 +1,21 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const {
+  addComment,
+  removeComment,
+  addReply,
+  removeReply
+} = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment)
 
-// // /api/pizzas
-// router.
-//   .route('/')
-//   .get(getAllPizza)
-//   .post(createPizza);
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
-// // /api/pizzas/:id
-// router
-//   .route('/:id')
-//   .get(getPizzaById)
-//   .put(updatePizza)
-//   .delete(deletePizza);
 
 module.exports = router;
